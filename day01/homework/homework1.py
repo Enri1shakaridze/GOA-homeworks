1e7851c3d547231ee628c825ce07a02bd29a6be0
enri2shakaridze <shakaradzeenrike361@gmail.com>
2025-04-23 19:12:17 +0400

    first commit

---
+++ day01/homework/homework1.py
@@ -0,0 +1,31 @@
+# ცვლადი არის სახელი რომლითაც ვინახავთ მონაცემს მაგ. name = "enrike"; age = 14
+# პითონში კომენტარის დანიშნულებაა რომ ის აადვილებს კოდის გარკვეული ნაწილის წაკითხვას.
+
+# მონაცემთა ტიპის 5-5 მაგალითი boolean ის შემთხვევაში 2.
+# string ()
+print("Python")
+print("1234")
+print("enrike")
+print("Hello world")
+print("Group 71")
+
+
+# integer (მთელი რიცხვი)
+print(10)
+print(12)
+print(0)
+print(2025)
+print(-5)
+
+
+# float 
+print(-5.1)
+print(10.5)
+print(14.5)
+print(39.5)
+print(-46.3)
+
+
+# Boolean 
+is_cold = True
+is_raining = False
