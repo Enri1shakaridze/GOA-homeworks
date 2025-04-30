3600d4913deab425a3df9cdbaeed800edb97de82
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-04-30 22:30:31 +0400

    first commit

---
+++ day 04/homework/main1.py
@@ -0,0 +1,20 @@
+# გავასწოროთ შეცდომები:
+# name = "Giorgi"
+# print(namme)
+name = "Giorgi"
+print(name)
+
+# number = 5
+# text = " apples"
+# result = number + text
+# print(result)
+num = "5"
+text = "apples"
+resoult = num + text
+print(resoult)
+
+# name = liKa
+# name2 = naame + "4"
+name = "lika"
+name2 = name + "4"
+
