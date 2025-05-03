dea1b2574e6ddfcb549e8c4b5719693268b41459
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-05-03 20:47:50 +0400

    first commit

---
+++ day 04/homework/main3.py
@@ -0,0 +1,6 @@
+# Debugging არის პროგრამაში შეცდომების (bug-ების) მოძიება და გამოსწორება.
+
+obj = "book"
+print(obj + "pages")  # გამოიტანს: bookpages
+obj2 = obj + "page"
+print(obj)  # გამოიტანს: book
