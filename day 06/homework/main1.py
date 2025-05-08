33a19cd999697aa1419e565f8314f621844ec0c5
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-05-08 18:25:31 +0400

    second commit

---
+++ day 06/homework/main1.py
@@ -0,0 +1,38 @@
+num1 = 10
+num2 = 5
+
+# + მიმატება (Addition)
+print(num1 + num2)
+
+# - გამოკლება (Subtraction)
+print(num1 - num2)
+
+# * გამრავლება (Multiplication)
+print(num1 * num2)
+
+# / გაყოფა (Division)
+print(num1 / num2)
+
+
+
+# first_user = "Lika"
+# 2nduser = "Giorgi" არასწორეა რადგან ცვლადის წინ ვერ დავწერთ რიცხვს
+# print(first user) არსწორეა რადგან ცვლადში გვიწერია snace case ით პრინტშიც უნდა გვეწეროს snace case ით
+first_user = "lika"
+second_user = "Giorgi"
+print(first_user)
+
+
+# first name = Data არასწორია რადგან არ გვაქ ბრჭყალები და snace case
+# last % name = "random" არასწორია რადგან snace case ის ნაცვლად პროცენტობა წერია
+first_name = "Data"
+last_name = "random"
+
+
+# 3. რას გამოიტანს ეს კოდი?
+# Num1 = 20
+# num2 = 30
+# print(num1 + num2) არასწორად წერია რადგან ცვლადში დიდი ასოთი წერია და პრინტში პატარათი
+num1 = 20
+num2 = 30
+print(num1 + num2) # გამოიტანს 50 ს.
