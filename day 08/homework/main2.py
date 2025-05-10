1bf6704698df5c3306b188b929d1b3a151063011
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-05-10 14:14:27 +0400

    second commit

---
+++ day 08/homework/main2.py
@@ -0,0 +1,9 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+# მათემატიკურ ოპერაციებს ვერ ჩავატარებთ რადგან input ყოველთვის ასრულებს სტრინგს.
+
