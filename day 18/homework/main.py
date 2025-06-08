7c47ded2401b871fb5ca102ba4ffed925482866b
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-06-08 12:00:17 +0400

    second commit

---
+++ day 18/homework/main.py
@@ -0,0 +1,19 @@
+for i in range(50, 200):
+    print(i)
+
+for i in range(1, 101):
+    print(i)
+
+for i in range(100, 150):
+    print(i)
+
+
+i = 1
+while i <+ 50:
+    print(i)
+    i += 1
+
+i = 20
+while i <+ 60:
+    print(i)
+    i += 5
---
+++ day 20/classwork/main1.py
@@ -0,0 +1,11 @@
+for i in range(11, 46, 2):
+    print(i)
+
+i = 20
+while i <= 50:
+    print(i)
+    i += 2
+
+
+for i in range(30, 1, -1):
+    print(i)
