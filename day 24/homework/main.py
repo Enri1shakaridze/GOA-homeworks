b6f483ff5332213b7298613ab816c49a9d620381
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-06-23 21:48:17 +0400

    second commit

--- day 24/homework/main.py
+++ day 24/homework/main.py
@@ -1,32 +1,61 @@
+<<<<<<< HEAD
 password = input("Enter your password: ")
 repeat = input("Repeat password: ")
+=======
+# List (ანუ სია) არის ისეთი მონაცემთა ტიპი, სადაც შენ შეგიძლია შეინახო ბევრი მნიშვნელობა ერთ ცვლადში. მაგალითად: რიცხვები, სიტყვები, True/False, სხვა ლისტებიც კი.
+password = input("Enter your password: ")
+repeat = input("Repeat password: ")
+
+>>>>>>> f98c6af (second commit)
 while repeat != password:
     repeat = input("Passwords do not match, try again: ")
 print("Password confirmed successfully")
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> f98c6af (second commit)
 secret_number = 7 
 guess = int(input("Try to guess the number: "))
 
 while guess != secret_number:
     print("Try again ")
     guess = int(input("Try again: "))
+<<<<<<< HEAD
 
+=======
+>>>>>>> f98c6af (second commit)
 print("You guessed the number successfully! ")
 
 
 
+<<<<<<< HEAD
 animals = ["lion", "tiger", "cat", "dog", "fox"]
 print("First and last animals:", animals[0], animals[-1])
 
 
 colors = ["black", "blue", "red", "green", "white"]
 print("The third item in the list is:", colors[-3])
+=======
+animals = ["lion", "tiger", "cat", "dog", "fox",]
+print(f"First and last animals:", animals[0], animals[-1])
+
+
+colors = ["black", "blue", "red", "green", "white"]
+print(f"The third item in the list is:", colors[-3])
+>>>>>>> f98c6af (second commit)
 
 
 
 name1 = input("Enter the first name: ")
 name2 = input("Enter the second name: ")
 name3 = input("Enter the third name: ")
+<<<<<<< HEAD
 
 print(name1, name2, name3)
+=======
+names = [name1, name2, name3]
+
+print(names)
+>>>>>>> f98c6af (second commit)
