ef508b1e53b1b713fa530f86d1f0b50b5e363b50
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-08-19 19:59:34 +0400

    second commit

---
+++ day 51/classwork/main.py
@@ -0,0 +1,12 @@
+# ვიმუშავეთ codewars- ებზე
+# 1) https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
+
+# 2) https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
+
+# 3) https://www.codewars.com/kata/56200d610758762fb0000002/train/python
+
+# 4) https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+# 5) https://www.codewars.com/kata/55a14f75ceda999ced000048/train/python
+
+# 6) https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
---
+++ day 51/homework/main.py
@@ -0,0 +1,35 @@
+# CODEWARS
+# 1
+def temple_strings(obj, feature): 
+    return f"{obj} are {feature}"
+
+
+# 2
+def find_average(numbers):
+    if len(numbers) != 0:
+        return sum(numbers) / len(numbers)
+    else:
+        return 0
+
+
+# 3
+def opposite(number):
+    return -number
+
+
+# 4
+def bool_to_word(boolean):
+    if boolean == True:
+        return 'Yes'
+    elif boolean == False:
+        return 'No'
+
+
+# 5
+def number_to_string(num):
+    return str(num)
+
+
+# 6
+def string_to_number(s):
+    return int(s)
