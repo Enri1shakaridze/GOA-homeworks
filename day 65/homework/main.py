3152d8eb6df938f0bc5ce275093f1ba650bfeb10
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2025-09-22 19:58:49 +0400

    second commit

---
+++ day 65/homework/main.py
@@ -0,0 +1,24 @@
+# 1) https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+
+def summation(num):
+    return num * (num + 1) // 2
+
+# 2) https://www.codewars.com/kata/55f1b763dd670651620000ce/train/python
+
+def count_char_occurrences(strng, char):
+    return strng.count(char)
+
+# 3) https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
+
+def array_plus_array(arr1,arr2):
+    return sum(arr1) + sum(arr2)
+
+# 4) https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+def no_space(x):
+    return x.replace(" ", "")
+
+# 5) https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/python
+
+def remove_every_other(my_list):
+    return my_list[::2]
