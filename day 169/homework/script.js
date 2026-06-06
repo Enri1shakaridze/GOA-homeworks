2d0434071d3c6a15d98b9a2a089c9a7d4209f85f
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2026-06-06 18:29:52 +0400

    day 169

--- day 169/homework/script.js
+++ day 169/homework/script.js
@@ -1,73 +1,88 @@
-
-const container = document.getElementById('container');
-const cart = document.getElementById('cart');
-const cart2 = document.querySelector('.cart2');
-const btn = document.getElementById('btn');
-const btn3 = document.getElementById('btn3');
-const p1 = document.getElementById('p1');
-const p2 = document.getElementById('p2');
-let res = [];
+let products = document.querySelector('.main-products');
+let kalata = document.querySelector('.kalata');
+let res = document.querySelector('.res');
+let res1 = document.querySelector('.res1');
+const pkalata = document.getElementById('pkalata');
 let price = 0;
-
-async function getfunc() {
-    try{
+let ln = 0;
+let array = [];
+async function getproducts(API) {
+    try {
         let response = await fetch('https://fakestoreapi.com/products');
         let data = await response.json();
         return data
     }
-    catch{
-        console.log('error')
+    catch (err){
+        console.log(err);
     }
 }
 
-btn.addEventListener('click', async () => {
-    let data2 = await getfunc();
-    cart.innerHTML = '';
-    cart2.id = 'cart2'
-    
-    data2.forEach(el => {
+async function screenproducts(){
+    let resdata = await getproducts();
+    console.log(resdata)
+    resdata.forEach(product => {
+        // div element(products)
+        let divall = document.createElement('div');
         let div = document.createElement('div');
-        div.innerHTML = `
-        <div id="div22">
-            <h1>${el.title}</h1>
-            <div id="divv1">
-                <img src="${el.image}"/>
-                <h3 id="pr">Price: $${el.price}</h3>
-            </div>
-        </div>
-        `
-    let btn2 = document.createElement('button');
-    btn2.textContent = 'კალათაში დამატება';
-    btn2.addEventListener('click', function(){
-        if(res.includes(div.innerHTML.replace('<button>კალათაში დამატება</button>', ''))){
-            alert('კალათაში უკვე დამატებულია!')
-            return
-        }
+        let div1 = document.createElement('div');
+        let div2 = document.createElement('div');
         
-        res.push(div.innerHTML.replace('<button>კალათაში დამატება</button>', ''));
-        cart.innerHTML = `${res.join('')}`
+        // products info
+        let ph2 = document.createElement('h2');
+        ph2.textContent = product.title;
+        ph2.id= 'ph2';
+        let pimg = document.createElement('img');
+        pimg.src = product.image;
+        let pp = document.createElement('p');
+        pp.textContent = `price is: $${product.price}`;
+        pp.id= 'pp';
+        let pbtn = document.createElement('button');
+        pbtn.id= 'pbtn';
+        pbtn.textContent = 'კალათაში დამატება';
 
-        p1.textContent = `სულ ${res.length} ერთეული`
-        price += el.price;
-        p2.textContent = `ფასი ჯამში $${price}`
-        
-    })
-    
-    
-    div.appendChild(btn2)
-    container.appendChild(div)
+        // product append div
+        div1.appendChild(ph2);
+        div2.appendChild(pimg);
+        div2.appendChild(pp);
+        div2.appendChild(pbtn);
+        div.appendChild(div1);
+        div.appendChild(div2);
+        divall.appendChild(div);
+        products.appendChild(divall);
+        div.id = "div";
+        div1.id = "div1";
+        div2.id = "div2";
+        divall.id = 'divall';
+        pbtn.addEventListener('click', function(){
+            if(array.includes(divall.innerHTML.replace('<button id="pbtn">კალათაში დამატება</button>', '<button id="rbtn">კალათიდან წაშლა</button>'))){
+                alert('ესეთი პროდუქტი უკვე დამატებულია!');
+                return;
+            }
+            array.push(divall.innerHTML.replace('<button id="pbtn">კალათაში დამატება</button>', '<button id="rbtn">კალათიდან წაშლა</button>'));
+            console.log(array);
+            res1.innerHTML = array.join('')
+            ln = array.length;
+            let rbtn = document.getElementById('rbtn');
+            
+            pkalata.textContent = ln;
+            if(ln >= 4){
+                res1.style.marginTop = '120px'
+                res1.style.marginBottom = '20px'
+            }
+        })
     });
-    
+}
+screenproducts();
+let tr = false;
+
+kalata.addEventListener('click', function(){
+    if(tr){
+        res.style.animation = 'none1 0.3s';
+        res.style.display = 'none';
+        tr = false;
+    }else{
+        res.style.display = 'flex';
+        res.style.animation = 'flexres 0.3s';
+        tr = true;
+    }
 })
-btn3.addEventListener('click', function() {
-    setTimeout(() => {
-        alert('წარმატებით შეიძინეთ!');
-
-        res = [];
-        cart.innerHTML = '';
-        price = 0;
-
-        p1.textContent = `სულ ${res.length} ერთეული`;
-        p2.textContent = `ფასი ჯამში ${price}`;
-    }, 500);
-});
