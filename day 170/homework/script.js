99aae61c62b9fb01fa8455933442554df108a0ee
Enri1shakaridze <shakaradzeenrike8@gmail.com>
2026-06-09 13:41:45 +0400

    day 170

--- day 170/homework/script.js
+++ day 170/homework/script.js
@@ -2,18 +2,21 @@ let products = document.querySelector('.main-products');
 let kalata = document.querySelector('.kalata');
 let res = document.querySelector('.res');
 let res1 = document.querySelector('.res1');
-const pkalata = document.getElementById('pkalata');
+let pkalata = document.getElementById('pkalata');
+let fullprice = document.getElementById('fullprice');
+let bynow = document.getElementById('bynow');
+let bynowtrue = document.getElementById('bynowtrue');
 let price = 0;
 let ln = 0;
 let array = [];
 let a;
+
 async function getproducts(API) {
     try {
         let response = await fetch('https://fakestoreapi.com/products');
         let data = await response.json();
         return data
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
     }
 }
@@ -21,7 +24,9 @@ async function getproducts(API) {
 async function screenproducts() {
     let resdata = await getproducts();
     console.log(resdata)
+
     resdata.forEach(product => {
+
         // div element(products)
         let divall = document.createElement('div');
         let div = document.createElement('div');
@@ -32,11 +37,14 @@ async function screenproducts() {
         let ph2 = document.createElement('h2');
         ph2.textContent = product.title;
         ph2.id = 'ph2';
+
         let pimg = document.createElement('img');
         pimg.src = product.image;
+
         let pp = document.createElement('p');
         pp.textContent = `price is: $${product.price}`;
         pp.id = 'pp';
+
         let pbtn = document.createElement('button');
         pbtn.id = 'pbtn';
         pbtn.textContent = 'კალათაში დამატება';
@@ -46,38 +54,85 @@ async function screenproducts() {
         div2.appendChild(pimg);
         div2.appendChild(pp);
         div2.appendChild(pbtn);
+
         div.appendChild(div1);
         div.appendChild(div2);
+
         divall.appendChild(div);
         products.appendChild(divall);
+
         div.id = "div";
         div1.id = "div1";
         div2.id = "div2";
         divall.id = 'divall';
+
         pbtn.addEventListener('click', function () {
+
             for (let i = 0; i < array.length; i++) {
                 if (array[i].id === product.id) {
-                    alert('ესეთი პროდუცქი უკვე არსებობს კალათაში!')
+                    alert('ესეთი პროდუცქი უკვე არსებობს კალათაში!');
                     return
                 }
             }
+
             array.push({
                 id: product.id,
-                
+                dv: divall.innerHTML.replace('<button id="pbtn">კალათაში დამატება</button>', '<button id="rbtn">კალათიდან წაშლა</button>'),
+                price: product.price
             });
-        
-            pkalata.textContent = ln;
-            if (ln >= 4) {
-                res1.style.marginTop = '120px'
-                res1.style.marginBottom = '20px'
+
+            console.log(array);
+
+            res1.innerHTML = '';
+            price = 0
+
+            for (let i = 0; i < array.length; i++) {
+                res1.innerHTML += array[i].dv;
+                price += array[i].price;
             }
-        })
+            fullprice.textContent = `ჯამში $${price}`;
+
+            ln = array.length;
+            console.log(array)
+
+            let rbtn = document.querySelectorAll('#rbtn');
+            rbtn = [...rbtn]
+            console.log(rbtn)
+            rbtn.forEach((remove, index) => {
+                remove.addEventListener('click', function () {
+
+                    array.splice(index, 1);
+                    console.log(array)
+                    res1.innerHTML = '';
+                    price = 0;
+
+                    for (let i = 0; i < array.length; i++) {
+                        res1.innerHTML += array[i].dv
+                        price += array[i].price
+                    }
+                    fullprice.textContent = `ჯამში $${price}`;
+
+                    pkalata.textContent = array.length
+                    console.log(remove)
+                });
+            });
+
+            pkalata.textContent = ln;
+
+            if (ln >= 4) {
+                res1.style.marginTop = '120px';
+                res1.style.marginBottom = '20px';
+            }
+        });
     });
 }
-screenproducts();
-let tr = false;
+
+screenproducts()
+
+let tr = false
 
 kalata.addEventListener('click', function () {
+
     if (tr) {
         res.style.animation = 'none1 0.3s';
         res.style.display = 'none';
@@ -86,5 +141,29 @@ kalata.addEventListener('click', function () {
         res.style.display = 'flex';
         res.style.animation = 'flexres 0.3s';
         tr = true;
+    }
+
+});
+
+bynow.addEventListener('click', function () {
+    if (array.length === 0) {
+        alert('კალათა ცარიელია!');
+        bynowtrue.style.display = 'none'
+
+        return
+    } else {
+        array = [];
+        price = 0;
+        ln = 0;
+
+
+        res1.innerHTML = ''
+        bynowtrue.style.display = 'inline'
+        setTimeout(() => {
+            bynowtrue.style.display = 'none'
+        }, 800);
+        fullprice.textContent = `ჯამში $${price}`;
+        pkalata.textContent = ln;
+
     }
 })
